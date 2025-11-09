1c1275e41ea2aab664ff412134c71b49cbc4582b
PremYekkali <yekkaliprem@gmail.com>
2025-11-09 14:35:41 +0530

    chore: configure hardhat compiler settings

---
+++ hardhat.config.ts
@@ -0,0 +1,17 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "solidity-coverage";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.26",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000
+      }
+    }
+  }
+};
+
+export default config;
